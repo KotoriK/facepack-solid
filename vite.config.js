e0ee91c1061c2a8b147103f5912a3bbf0db37346
KotoriK <52659125+KotoriK@users.noreply.github.com>
2022-12-08 10:07:40 +0800

    chore: update build setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,20 @@
 // vite.config.ts
 import { defineConfig } from 'vite';
 import solidPlugin from 'vite-plugin-solid';
+import path from 'node:path'
 
 export default defineConfig({
   plugins: [solidPlugin()],
+  build: {
+    lib: {
+      entry: path.resolve(__dirname, 'src/index.ts'),
+      formats: ['es', 'cjs'],
+      // the proper extensions will be added
+      fileName: 'FacePack',
+    },
+    target: 'esnext',
+    minify: false,
+    reportCompressedSize: false,
+    sourcemap: true
+  },
 });
