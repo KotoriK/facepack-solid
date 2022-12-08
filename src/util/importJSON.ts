71f83bc63fb88e52ce3918b39febbdedace4fd1b
KotoriK <52659125+KotoriK@users.noreply.github.com>
2022-12-08 10:07:20 +0800

    refactor: remove unuse async

--- src/util/importJSON.ts
+++ src/util/importJSON.ts
@@ -1,9 +1,11 @@
-export async function importJSON(url:string){
-    return fetch(url,{method:"GET"}).then((response)=>{
-        if(response.ok){
-            return response.json()
-        }else{
-            throw new Error(`Fetch Error:Return HTTP ${response.status}, ${response.statusText}`)
-        }
-    })
-}
+export const importJSON =
+    (url: string) => fetch(url)
+        .then(
+            (response) => {
+                if (response.ok) {
+                    return response.json()
+                } else {
+                    throw new Error(`Fetch Error:Return HTTP ${response.status}, ${response.statusText}`)
+                }
+            }
+        )
